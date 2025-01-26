6a6f943e75cc6935f8e84e3d2d8f70dcb7eea8b1
216037150 <siyabongazungu95@gmail.com>
2025-01-26 03:39:47 +0200

    Added questions

--- question1.py
+++ question1.py
@@ -3,6 +3,11 @@ import pandas as pd
 # Read the CSV data
 df = pd.read_csv('ApplianceWorld.csv', delimiter=';')
 
+
+print(df.head)
+
+# print(df["Revenue"].isnull())
+
 # Filter for Quarter 1 of 2023 (January, February, March)
 q1_2023 = df[(df['Year'] == 2023) & (df['Month No'].isin([1, 2, 3]))]
 
